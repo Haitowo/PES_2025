acc4622ac2fb7d4a47a2176d435b852fc321e26c
Florian MAJCHER <f.majcher@student.isartdigital.com>
2026-02-15 15:36:17 +0100

    [ADD] Sun + Circular movements

---
+++ PES - Project/Assets/Scripts/PlanetManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Author : Florian MAJCHER - Isart DIGITAL
+// DATE : 00/00/0000 - Beginning of the class
+
+namespace Com.IsartDigital.PES.Planet
+{
+    
+    public class PlanetManager : MonoBehaviour
+    {
+        // ----------------~~~~~~~~~~~~~~~~~~~==========================# // VARIABLES
+        [SerializeField] private float _Radius = 5f;
+        [SerializeField] private float _Speed = 10f;
+
+        [SerializeField] private Transform _Center;
+
+        // ----------------~~~~~~~~~~~~~~~~~~~==========================# // READY
+        private void Awake()
+        {
+
+        }
+
+        // ----------------~~~~~~~~~~~~~~~~~~~==========================# // PROCESS
+        private void Update()
+        {
+            CircleMovement();
+        }
+
+        private void CircleMovement()
+        {
+            float lAngle = _Speed * Time.time;
+            float lX = Mathf.Cos(lAngle) * _Radius;
+            float lZ = Mathf.Sin(lAngle) * _Radius;
+
+            transform.position = new Vector3(lX, transform.position.y, lZ);
+        }
+    }
+}
